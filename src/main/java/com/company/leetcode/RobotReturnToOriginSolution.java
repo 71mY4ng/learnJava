6b3380f92449459100731080bda3651a4b06a049
Tim <1648746779@qq.com>
2020-08-29 00:31:51 +0800

    update robot-return-to-origin: avoid over design

--- src/main/java/com/company/leetcode/RobotReturnToOriginSolution.java
+++ src/main/java/com/company/leetcode/RobotReturnToOriginSolution.java
@@ -1,77 +1,28 @@
 package com.company.leetcode;
 
-import java.util.Optional;
-
 public class RobotReturnToOriginSolution {
-
-    enum Action {
-
-        /** */
-        R(new PositionXY(1, 0)),
-        /** */
-        L(new PositionXY(-1, 0)),
-        /** */
-        U(new PositionXY(0, 1)),
-        /** */
-        D(new PositionXY(0, -1)),
-        /** */
-        NOACTION(new PositionXY(0, 0)),
-        ;
-
-        private PositionXY change;
-
-        Action(PositionXY change) {
-            this.change = change;
-        }
-
-        public static Optional<Action> parse(String actionKey) {
-            for (Action action : Action.values()) {
-                if (action.name().equalsIgnoreCase(actionKey)) return Optional.of(action);
-            }
-            return Optional.empty();
-        }
-    }
-
-    static class PositionXY {
-        int x;
-        int y;
-
-        public PositionXY() {
-            this.x = 0;
-            this.y = 0;
-        }
-
-        public PositionXY(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public boolean atStartingPoint() {
-            return (x == 0 && y == 0);
-        }
-
-        public void move(Action action) {
-            this.x += action.change.x;
-            this.y += action.change.y;
-        }
-    }
 
 
     public boolean judgeCircle(String moves) {
 
-        final PositionXY robotPosition = new PositionXY();
+        int x = 0, y = 0;
         for (char move : moves.toCharArray()) {
-            robotPosition.move(
-                    Action.parse(String.valueOf(move))
-                            .orElse(Action.NOACTION));
+            switch (move) {
+                case 'L': x--; break;
+                case 'R': x++; break;
+                case 'U': y++; break;
+                case 'D': y--; break;
+                default:
+                    break;
+            }
         }
 
-        return robotPosition.atStartingPoint();
+        return x == 0 && y == 0;
     }
 
     public static void main(String[] args) {
 
         final RobotReturnToOriginSolution a = new RobotReturnToOriginSolution();
-        System.out.println(a.judgeCircle("LL"));
+        System.out.println(a.judgeCircle("UD"));
     }
 }
