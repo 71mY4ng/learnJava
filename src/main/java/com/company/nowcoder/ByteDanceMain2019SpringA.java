1258740d152754edb0b41265fc18d23cbfe0fe4d
Tim <1648746779@qq.com>
2020-09-21 18:38:33 +0800

    submit ByteDanceMain2019SpringA

---
+++ src/main/java/com/company/nowcoder/ByteDanceMain2019SpringA.java
@@ -0,0 +1,63 @@
+package com.company.nowcoder;
+
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class ByteDanceMain2019SpringA {
+
+    static class SolverA {
+
+        public void solve(String s) {
+
+            int wordLen = s.length();
+            char[] chars = s.toCharArray();
+
+            for (int j = 0; j < wordLen; j++) {
+
+                if (j + 1 < wordLen && chars[j] == chars[j + 1]) {
+
+                    // AAA -> _↓AA
+                    if (j + 2 < wordLen && chars[j] == chars[j + 2]) {
+                        chars[j] = ' ';
+                        continue;
+                    }
+
+                    // AABB -> AAB_↓
+                    if (j + 3 < wordLen && chars[j + 2] == chars[j + 3]) {
+                        chars[j + 2] = ' ';
+                        j += 3;
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for (char c : chars) {
+                if (c != ' ')
+                    sb.append(c);
+            }
+
+            out.print(sb.toString());
+        }
+    }
+
+    public static void main(String[] argv) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int n = sc.nextInt();
+            sc.useDelimiter("\n");
+            sc.nextLine();
+
+            final SolverA solverA = new SolverA();
+            while (sc.hasNext()) {
+                String inputStr = sc.nextLine();
+
+                solverA.solve(inputStr);
+
+                if (sc.hasNext()) out.println();
+            }
+
+        }
+    }
+
+}
